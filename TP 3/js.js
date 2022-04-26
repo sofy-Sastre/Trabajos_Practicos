aa8f20c9272d710e465a948401d3aacad2beb530
sofy-Sastre <89938756+sofy-Sastre@users.noreply.github.com>
2022-04-26 03:29:29 -0300

    Add files via upload

---
+++ TP 3/js.js
@@ -0,0 +1,9 @@
+var principal = document.getElementById('Prueba_sofi')
+
+var Etiquetah1 = document.createElement('h1')
+
+console.log(Etiquetah1)
+
+Etiquetah1.innerHTML = "Trabajo Práctico 3"
+
+Prueba_sofi.appendChild(Etiquetah1)
